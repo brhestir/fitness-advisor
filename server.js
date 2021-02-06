067d156aa8727b34a50c402621f4a34633e6dc9b
Brian Hestir <brhestir@gmail.com>
2021-02-06 13:20:28 -0500

    Outlining workout model

--- server.js
+++ server.js
@@ -1,2 +1,41 @@
 // server.js
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const { response } = require("express");
 
+const PORT = process.env.PORT || 3000;
+
+// Require models
+
+// create express instance
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+// app.post("/arbitraryRoute", ({body}, res) => {
+//     ModelName.create(body)
+//     .then(dbModelName => { 
+//         res.json(dbModelName);
+//     })
+//     .catch(err => {
+//         res.json(err);
+//     });
+// });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
