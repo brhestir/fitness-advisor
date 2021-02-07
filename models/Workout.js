5b7a07b8f8686cbacdc0f9a7ece031b2c04a3d80
Brian Hestir <brhestir@gmail.com>
2021-02-07 13:26:33 -0500

    Added ESLint

--- models/Workout.js
+++ models/Workout.js
@@ -1,14 +1,16 @@
+"use strict";
 // Workout.js
 
 const mongoose = require("mongoose");
+const { Exercise } = require("../models");
 
 const Schema = mongoose.Schema;
 
 // Mongoose - arrays of subdocuments
 // The Workout schema contains an array of Exercise schema.
 const WorkoutSchema = new Schema({
-  day: { type: Date },
-  exercises: [ExerciseSchema],
+	day: { type: Date },
+	exercises: [Exercise]
 });
 
 // Calling mongoose.model() on a schema causes mongoose to COMPILE a model for us
